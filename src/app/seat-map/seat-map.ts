50aaebd3c1a9fbd26f782f6e576a2a4211b8fc1a
eslam emam <89427236+eslamemam1@users.noreply.github.com>
2025-07-05 01:29:16 +0300

    add seat map and demo info in home page

--- src/app/seat-map/seat-map.ts
+++ src/app/seat-map/seat-map.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-seat-map',
-  imports: [],
+  standalone: true,
   templateUrl: './seat-map.html',
-  styleUrl: './seat-map.css'
+  styleUrls: [],
+  imports: [CommonModule],
 })
 export class SeatMap {
+  rows = 5;
+  cols = 8;
 
+  seats: { status: 'available' | 'reserved' | 'selected' }[][] = [];
+
+  constructor() {
+    for (let i = 0; i < this.rows; i++) {
+      const row = [];
+      for (let j = 0; j < this.cols; j++) {
+        row.push({
+          status: 'available' as 'available' | 'reserved' | 'selected',
+        });
+      }
+      this.seats.push(
+        row as { status: 'available' | 'reserved' | 'selected' }[]
+      );
+    }
+  }
+
+  toggleSeat(rowIndex: number, colIndex: number) {
+    const seat = this.seats[rowIndex][colIndex];
+    if (seat.status === 'reserved') return;
+
+    seat.status = seat.status === 'selected' ? 'available' : 'selected';
+  }
 }
