694bd629ffdced0e873b16211be843a45c8ff672
eslam emam <89427236+eslamemam1@users.noreply.github.com>
2025-07-05 00:23:36 +0300

    header update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { SeatMap } from './seat-map/seat-map';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'seat-map',
+    component: SeatMap,
+  },
+];
---
+++ src/app/seat-map/seat-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seat-map',
+  imports: [],
+  templateUrl: './seat-map.html',
+  styleUrl: './seat-map.css'
+})
+export class SeatMap {
+
+}
